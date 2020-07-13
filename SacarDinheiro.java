f627ae4d795ad5e95795c402162d0db7974a1ee8
Monica <mnoritake@gmail.com>
2020-07-12 21:03:19 -0300

    banco

---
+++ SacarDinheiro.java
@@ -0,0 +1,54 @@
+package implementacao;
+
+public class SacarDinheiro {
+
+	public static void main(String[] args) {
+		
+	String variavel= "asdasdasda";
+variavel = 				JOptionPane.showInputDialog("Digite Nome do Cliente"),
+
+	
+		
+		
+	/*	Cliente cliente = new Cliente(
+				Integer.parseInt(JOptionPane.showInputDialog("Digite Codigo do Cliente")),
+				JOptionPane.showInputDialog("Digite Nome do Cliente"),
+				JOptionPane.showInputDialog("Digite E-mail"),
+				JOptionPane.showInputDialog("Digite Fone"),
+				JOptionPane.showInputDialog("Digite CPF"),
+				new Endereco(JOptionPane.showInputDialog("Digite Logradouro"),
+						JOptionPane.showInputDialog("Digite bairro"),
+						JOptionPane.showInputDialog("Digite Cidade"),
+						JOptionPane.showInputDialog("Digite CEP"),
+						JOptionPane.showInputDialog("Digite numero"),
+						JOptionPane.showInputDialog("Digite UF"),
+						JOptionPane.showInputDialog("Digite Complemento")	
+					)
+				);
+	
+		Conta conta = new Conta(
+				JOptionPane.showInputDialog("Digite numero da Conta:"),
+				JOptionPane.showInputDialog("Digite Agencia:"),
+				JOptionPane.showInputDialog("Digite Cliente:"),
+				JOptionPane.showInputDialog("Informe Saldo:")
+				
+				
+				);
+		
+		Agencia agencia = new Agencia();
+		
+		
+		
+		Endereco endereco = new Endereco();
+		
+		
+		
+		
+		
+		
+		
+	*/	
+		
+	}
+
+}
